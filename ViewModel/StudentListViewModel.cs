577f2932c17834d1759fb276b5998a65fb4fb2da
alok-urmaliya <alokurmaliya0123@gmail.com>
2023-06-22 10:33:09 +0530

    updated connection string, List page layout "WIP"

--- ViewModel/StudentListViewModel.cs
+++ ViewModel/StudentListViewModel.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+﻿using App01.Model;
+using App01.Services;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
 using Microsoft.Toolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,28 @@ namespace App01.ViewModel
 {
     public partial class StudentListViewModel : ObservableObject
     {
+        private readonly IStudentService _studentService;
+
+        public ObservableCollection<Student> Students { get; set; } = new ObservableCollection<Student>();
+        public StudentListViewModel(IStudentService studentService) 
+        {
+            _studentService = studentService;
+        }
+
+        [ICommand]
+        public void GetStudentList()
+        {
+            var studentList = _studentService.GetStudents();
+            if(studentList?.Count > 0)
+            {
+                Students.Clear();
+                foreach (var student in studentList) 
+                {
+                    Students.Add(student);
+                }
+            }
+        }
+
         [ICommand]
         public async void AddStudentButton()
         {
